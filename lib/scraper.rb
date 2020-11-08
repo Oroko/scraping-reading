24a3f28b7af138a9a5680c1a1e771e948d87dddb
Oroko <aoroko09@gmail.com>
2020-11-08 10:33:13 -0500

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,15 @@
+# frozen_string_literal: true
+
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
-html = open("https://flatironschool.com/")
+html = open('https://flatironschool.com/')
+
+doc = Nokogiri::HTML(html)
+puts doc.css('.headline-26OIBN').text.strip
+courses = doc.css('.inlineMobileLeft-2Yo002 .imageTextBlockGrid2-3jXtmC')
+
+courses.each do |course|
+  puts course.text.strip
+end
